7cd9d2930b5a98a1cac1ce83331e11abfc3ac549
Ashrafelsolyy <1ashraf2019@gmil.com>
2024-03-06 17:12:58 +0200

    first commit

---
+++ js/min.js
@@ -0,0 +1,6 @@
+var icon = document.querySelectorAll('.fa-heart');
+var position = document.querySelectorAll('.position' );
+
+position.addEventListener('muoseover', function(){
+    icon.style.display = "block";
+})
